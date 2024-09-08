beb587fd8009c9f53c3104f194d5f40a5d870ee7
SemiScientist. <165859494+deseafiles@users.noreply.github.com>
2024-09-08 17:24:43 +0800

    Update Main.java

--- app/src/main/java/linkedlist/Main.java
+++ app/src/main/java/linkedlist/Main.java
@@ -1,136 +1,166 @@
-package linkedlist;
+package main.java.com.example.Stack;
 
-class Node<T> {
-    private T value;
-    private Node<T> next;
-
-    public Node(T value) {
-        this.value = value;
-        this.next = null;
-    }
-
-    public void setNext(Node<T> next) {
-        this.next = next;
-    }
-
-    public Node<T> getNext(){
-        return next;
-    }
-
-    public T getValue() {
-        return value;
-    }
-}
-
-class Stack<T> {
-    private Node<T> first;
-
-    public Stack() {
-        this.first = null;
-    }
-
-    public void push(T value) {
-        Node<T> newNode = new Node<>(value);
-        newNode.setNext(first); 
-        first = newNode; 
-    }
-
-    public boolean hasPop() {
-        if (first != null) {
-            System.out.println("Stack tidak kosong!");
-            return true;
-        } else {
-            System.out.println("Stack Kosong!");
-            return false;
-        }
-    }
-
-    public T pop() {
-        if (first == null) {
-            System.out.println("Stack Kosong Tidak Dapat Menghapus Elemen");
-            return null; 
-        } else {
-            Node<T> currentNode = first;
-            T poppedValue = currentNode.getValue();
-            first = currentNode.getNext();
-            return poppedValue;
-        }
-    }
-
-    public void swap(int index1, int index2) {
-        int count = 0;
-        Node<T> currentNode = first;
-        while (currentNode != null) {
-            count++;
-            currentNode = currentNode.getNext();
-        }
-
-        if (index1 < 0 || index2 < 0 || index1 >= count || index2 >= count) {
-            System.out.println("Index tidak valid");
-            return;
-        }
-
-        if (index1 == index2) {
-            return;
-        }
-
-        Node<T> prev1 = null, prev2 = null;
-        Node<T> node1 = first, node2 = first;
-
-        for (int i = 0; i < count - index1 - 1; i++) {
-            prev1 = node1;
-            node1 = node1.getNext();
-        }
-
-        for (int i = 0; i < count - index2 - 1; i++) {
-            prev2 = node2;
-            node2 = node2.getNext();
-        }
-
-        if (prev1 != null) {
-            prev1.setNext(node2);
-        } else {
-            first = node2;
-        }
-
-        if (prev2 != null) {
-            prev2.setNext(node1);
-        } else {
-            first = node1;
-        }
-
-        Node<T> temp = node1.getNext();
-        node1.setNext(node2.getNext());
-        node2.setNext(temp);
-    }
-
-    public void printList() {
-        Node<T> currentNode = first;
-        
-        while (currentNode != null) {
-            System.out.print(" <- " + currentNode.getValue());
-            currentNode = currentNode.getNext();
-        }
-        System.out.println();
-        System.out.println("-------------------------------");
-    }
-}
+import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> dataInt = new Stack<>();
-        Stack<Character> dataChar = new Stack<>();
+        Stack<Integer> stackInt = new Stack<>();
+        Stack<Character> stackChar = new Stack<>();
+        Queue queue = new Queue();
+        Scanner scanner = new Scanner(System.in);
+        int choice, index1, index2;
+        Integer intValue;
+        Character charValue;
 
-        dataInt.hasPop(); // Memeriksa apakah stack tidak kosong
-        dataInt.printList(); // Menampilkan stack (harus kosong pada awalnya)
+        while (true) {
+            System.out.println("Choose an operation:");
+            System.out.println("1. Push Integer to Stack");
+            System.out.println("2. Push Character to Stack");
+            System.out.println("3. Pop from Stack");
+            System.out.println("4. Swap elements in Stack");
+            System.out.println("5. Peek to Stack");
+            System.out.println("6. Enqueue to Queue");
+            System.out.println("7. Dequeue from Queue");
+            System.out.println("8. Display Stack");
+            System.out.println("9. Display Queue");
+            System.out.println("10. Swap elements in Queue");
+            System.out.println("11. Peek to Queue");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
 
-        dataInt.push(1); 
-        dataInt.push(2);
-        dataInt.push(3);
-        dataInt.hasPop(); // Memeriksa apakah stack tidak kosong
-        dataInt.printList(); // Menampilkan stack setelah push
-
-        dataInt.swap(0, 1); // Menukar elemen pada indeks 0 dan 1
-        dataInt.printList(); // Menampilkan stack setelah swap
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Integer value to push to stack: ");
+                    intValue = scanner.nextInt();
+                    stackInt.push(intValue);
+                    break;
+                case 2:
+                    System.out.print("Enter Character value to push to stack: ");
+                    charValue = scanner.next().charAt(0);
+                    stackChar.push(charValue);
+                    break;
+                case 3:
+                    System.out.println("Pop from which stack? (1 for Integer, 2 for Character): ");
+                    int stackChoice = scanner.nextInt();
+                    if (stackChoice == 1) {
+                        try {
+                            intValue = stackInt.pop();
+                            System.out.println("Popped value from stack (Integer): " + intValue);
+                        } catch (EmptyStackException e) {
+                            System.out.println("Stack is empty");
+                        }
+                    } else if (stackChoice == 2) {
+                        try {
+                            charValue = stackChar.pop();
+                            System.out.println("Popped value from stack (Character): " + charValue);
+                        } catch (EmptyStackException e) {
+                            System.out.println("Stack is empty");
+                        }
+                    } else {
+                        System.out.println("Invalid stack choice");
+                    }
+                    break;
+                case 4:
+                    System.out.print("Enter first index to swap in stack: ");
+                    index1 = scanner.nextInt();
+                    System.out.print("Enter second index to swap in stack: ");
+                    index2 = scanner.nextInt();
+                    try {
+                        if (stackInt.size() > 0) {
+                            stackInt.swap(index1, index2);
+                            System.out.println("After swap: " + stackInt);
+                        } else {
+                            stackChar.swap(index1, index2);
+                            System.out.println("After swap: " + stackChar);
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Exception during swap: " + e.getMessage());
+                    }
+                    break;
+                case 5:
+                    System.out.println("Peek from which stack? (1 for Integer, 2 for Character): ");
+                    int stackPeekChoice = scanner.nextInt();
+                    if (stackPeekChoice == 1) {
+                        try {
+                            intValue = stackInt.peek();
+                            System.out.println("Top value in stack (Integer): " + intValue);
+                        } catch (EmptyStackException e) {
+                            System.out.println("Stack is empty");
+                        }
+                    } else if (stackPeekChoice == 2) {
+                        try {
+                            charValue = stackChar.peek();
+                            System.out.println("Top value in stack (Character): " + charValue);
+                        } catch (EmptyStackException e) {
+                            System.out.println("Stack is empty");
+                        }
+                    } else {
+                        System.out.println("Invalid stack choice");
+                    }
+                    break;
+                case 6:
+                    System.out.print("Enter value to enqueue to queue: ");
+                    charValue = scanner.next().charAt(0);
+                    queue.enqueue(charValue);
+                    break;
+                case 7:
+                    try {
+                        charValue = queue.dequeue();
+                        System.out.println("Dequeued value from queue: " + charValue);
+                    } catch (NoSuchElementException e) {
+                        System.out.println("Queue is empty");
+                    }
+                    break;
+                case 8:
+                    System.out.println("Stack (Integer): " + stackInt);
+                    System.out.println("Stack (Character): " + stackChar);
+                    break;
+                case 9:
+                    System.out.println("Queue: " + queue);
+                    break;
+                case 10:
+                    System.out.print("Enter first index to swap in queue: ");
+                    index1 = scanner.nextInt();
+                    System.out.print("Enter second index to swap in queue: ");
+                    index2 = scanner.nextInt();
+                    try {
+                        queue.swap(index1, index2);
+                        System.out.println("After swap: " + queue);
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Exception during swap: " + e.getMessage());
+                    }
+                    break;
+                case 11:
+                    System.out.println("Peek from which queue? (1 for Front, 2 for Rear): ");
+                    int queuePeekChoice = scanner.nextInt();
+                    if (queuePeekChoice == 1) {
+                        try {
+                            charValue = queue.peekFront();
+                            System.out.println("Front value in queue: " + charValue);
+                        } catch (NoSuchElementException e) {
+                            System.out.println("Queue is empty");
+                        }
+                    } else if (queuePeekChoice == 2) {
+                        try {
+                            charValue = queue.peekRear();
+                            System.out.println("Rear value in queue: " + charValue);
+                        } catch (NoSuchElementException e) {
+                            System.out.println("Queue is empty");
+                        }
+                    } else {
+                        System.out.println("Invalid queue choice");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Exiting program...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
